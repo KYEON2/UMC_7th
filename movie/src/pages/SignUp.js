7b888dd737cab15bba67267176637e5fe2222b8c
Kiyeon Roh <rohkiyeon@naver.com>
2024-11-03 01:11:01 +0900

    chapter5

--- movie/src/pages/SignUp.js
+++ movie/src/pages/SignUp.js
@@ -1,21 +1,111 @@
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import useForm from "../hooks/useForm";
+import { validateSignUp, validateUser } from "../utils/validate";
 
 const Con = styled.div`
     background-color: black;
+    font-size: 20px;
+    font-weight: 600;
     color: white;
     height: 100vh; 
     width: 100vw; 
     display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+`;
+
+const InputCon = styled.div`
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 `;
 
-function SignUp() {
+const Input = styled.input`
+  width: 300px;
+  height: 35px;
+  box-sizing: border-box;
+  margin: 5px 0px;
+  border-radius: 7px;
+  border: ${props => props.error ? '2px solid red' : '1px solid #ccc'};
+`;
+
+const Button = styled.button`
+  width: 300px;
+  height: 35px;
+  box-sizing: border-box;
+  background-color: #FA0049;
+  border-radius: 7px;
+  margin: 5px 0px;
+  border: 0px;
+`;
+
+const ErrorText = styled.p`
+  color: red;
+  font-size: 12px;
+`;
+
+function Signup() {
+  const signup = useForm({
+    initialValue: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    },
+    validate: (values) => {
+      const errors = validateSignUp(values);
+      return errors;
+    }
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (Object.keys(signup.errors).length === 0) {
+      alert("회원가입 성공!");
+      // 회원가입 처리 로직 추가
+    }
+  };
+
   return (
     <Con>
-      SignUp
+      회원가입
+      <InputCon>
+        <Input
+          error={signup.touched.name && signup.errors.name}
+          type="text"
+          {...signup.getTextInputProps("name")}
+          placeholder="이름을 입력하세요"
+        />
+        {signup.touched.name && signup.errors.name && (<ErrorText>{signup.errors.name}</ErrorText>)}
+        <Input
+          error={signup.touched.email && signup.errors.email}
+          type="email"
+          {...signup.getTextInputProps("email")}
+          placeholder="이메일을 입력하세요"
+        />
+        {signup.touched.email && signup.errors.email && (<ErrorText>{signup.errors.email}</ErrorText>)}
+        <Input
+          error={signup.touched.password && signup.errors.password}
+          type="password"
+          {...signup.getTextInputProps("password")}
+          placeholder="비밀번호를 입력하세요"
+        />
+        {signup.touched.password && signup.errors.password && (<ErrorText>{signup.errors.password}</ErrorText>)}
+        <Input
+          error={signup.touched.confirmPassword && signup.errors.confirmPassword}
+          type="password"
+          {...signup.getTextInputProps("confirmPassword")}
+          placeholder="비밀번호 확인"
+        />
+        {signup.touched.confirmPassword && signup.errors.confirmPassword && (<ErrorText>{signup.errors.confirmPassword}</ErrorText>)}
+        <Button onClick={handleSubmit}>회원가입</Button>
+      </InputCon>
     </Con>
   );
 }
 
-export default SignUp;
+export default Signup;
--- movie/src/utils/validate.js
+++ movie/src/utils/validate.js
@@ -1,24 +1,35 @@
 const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
 
-function validateUser (values) {
-    const errors = {
-        email: '',
-        password: '',
+function validateUser(values) {
+    const errors = {};
+
+    if (!values.email || emailPattern.test(values.email) === false) {
+        errors.email = '올바른 이메일 형식이 아닙니다. 다시 확인해주세요';
     }
 
-    if (emailPattern.test(values.email)=== false){
-        errors.email = '올바른 이메일 형식이 아닙니다. 다시 확인해주세요'
-    }
-
-    if (values.password.length < 8 || values.password.length > 16) {
-        errors.password = '비밀번호는 8 ~ 16자 사이로 입력해주세요'
+    if (!values.password || values.password.length < 8 || values.password.length > 16) {
+        errors.password = '비밀번호는 8 ~ 16자 사이로 입력해주세요';
     }
 
     return errors;
 }
 
-function validateLogin (values) {
+function validateLogin(values) {
     return validateUser(values);
 }
 
-export {validateLogin}
+function validateSignUp(values) {
+    const errors = validateUser(values);
+
+    if (!values.name) {
+        errors.name = "이름을 입력해주세요.";
+    }
+
+    if (values.password !== values.confirmPassword) {
+        errors.confirmPassword = "비밀번호가 일치하지 않습니다.";
+    }
+
+    return errors;
+}
+
+export { validateLogin, validateSignUp };
